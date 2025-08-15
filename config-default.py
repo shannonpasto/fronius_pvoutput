c6cd3d6be4755d9046146f5627287b36abd6c63e
Shannon Pasto <shannonpasto@me.com>
2025-08-15 17:39:01 +1000

    updates
    new timezone option in config.py
    added tzlocal to requirements.txt
    main.py changes...
    changed shebang
    PEP 8 compliance
    using base URL for PVOutput
    fixed a couple of spelling mistakes
    exits if API data from the inverter fails
    better handling of timestamp. uses inverter timestamp
    uses inverter timestamp to determine timezone, uses timezone set on config.py or local system

--- config-default.py
+++ config-default.py
@@ -1,3 +1,7 @@
+# mandatory
 inverter_addr = ""  # ip or hostname of your inverter
 pvo_sid = ""  # pvo system id
 pvo_api_key = ""  # pvo api key
+
+# optional
+timezone = ""  # your local timezone, eg Australia/Melbourne
--- main.py
+++ main.py
@@ -1,15 +1,22 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
-import requests
 import sys
 from urllib.parse import urlencode
-from datetime import datetime
+from datetime import datetime, timezone
+from zoneinfo import ZoneInfo
+
+import requests
+import tzlocal
+
 import config as cfg
 
-pvo_url = "https://pvoutput.org/service/r2/addstatus.jsp"
+PVO_BASE_URL = "https://pvoutput.org"
 
 
 def get_data(url):
+    """Fetch JSON from the URL with a 10s timeout; raises for HTTP errors.
+    Returns JSON response
+    """
     try:
         response = requests.get(url, timeout=10)
         response.raise_for_status()
@@ -23,12 +30,16 @@ def get_data(url):
 
 
 def get_num(data, path):
+    """Ensure the data from the JSON is a number; raises for
+    KeyError and TypeError errors and exists.
+    Input is: JSON, path to key
+    """
     try:
         for key in path:
             data = data[key]
     except (KeyError, TypeError) as e:
         print(f"Error retrieving {'.'.join(path)}: {e}")
-        sys.exit(1)  # stop script
+        sys.exit(1)
 
     if data is None:
         print(f"Value at {'.'.join(path)} is None")
@@ -40,6 +51,8 @@ def get_num(data, path):
         return data
 
 
+# start the script here
+# check for the required config options
 req_attr = [
     ("inverter_addr", cfg.inverter_addr),
     ("pvo_sid", cfg.pvo_sid),
@@ -61,55 +74,80 @@ if len(missing_attr) > 0:
     print()
     sys.exit(1)
 
-interter_realtime_data = get_data(
+# get the json data from the inverter
+inverter_realtime_data = get_data(
     f"http://{cfg.inverter_addr}/solar_api/v1/"
     "GetInverterRealtimeData.cgi?Scope=Device&DataCollection=CommonInverterData"
 )
+if inverter_realtime_data is None:
+    sys.exit(1)
 
+# get the json data from the smart meter
 power_flow_realtime_data = get_data(
     f"http://{cfg.inverter_addr}/solar_api/v1/GetPowerFlowRealtimeData.fcgi"
 )
+if power_flow_realtime_data is None:
+    sys.exit(1)
 
-# v2
+# get the required values from the inverter and smart meter
+# https://pvoutput.org/help/api_specification.html#add-status-service
+
+# v2 - Power Generation
 power_generation = get_num(
     power_flow_realtime_data,
     ["Body", "Data", "Site", "P_PV"]
 )
 
-# v4
+# v4 - Power Consumption
 power_consumption = -get_num(
     power_flow_realtime_data,
     ["Body", "Data", "Site", "P_Load"]
 )
 
-# v6
+# v6 - Voltage
 voltage = get_num(
-    interter_realtime_data,
+    inverter_realtime_data,
     ["Body", "Data", "UAC", "Value"]
 )
 
+# inverter timestamp
+inverter_timestamp = inverter_realtime_data["Head"]["Timestamp"]
+
+# check timestamp is in local time. convert if needed
+dt = datetime.fromisoformat(inverter_timestamp)
+
+tz_name = getattr(cfg, "timezone", None) or tzlocal.get_localzone_name()
+site_tz = ZoneInfo(tz_name)
+
+if dt.utcoffset() == timezone.utc.utcoffset(dt):
+    dt = dt.astimezone(site_tz)
+
+# set the URL parameters
+# c1=2 as v1 is total energy, ie cumulative value
 params = {
-    "d": datetime.today().strftime('%Y%m%d'),
-    "t": datetime.today().strftime('%H:%M'),
-    "v2": f"{power_generation:.2f}",
-    "v4": f"{power_consumption:.2f}",
-    "v6": round(voltage, 2)
+    "d": dt.strftime("%Y%m%d"),
+    "t": dt.strftime("%H:%M"),
+    "v2": int(round(power_generation)),
+    "v4": int(round(power_consumption)),
+    "v6": round(voltage, 1)
 }
 
+# make the PVOutput POST
 try:
     response = requests.post(
-        pvo_url,
+        f"{PVO_BASE_URL}/service/r2/addstatus.jsp",
         headers={
             'X-Pvoutput-Apikey': f'{cfg.pvo_api_key}',
             'X-Pvoutput-SystemId': f'{cfg.pvo_sid}',
         },
-        params=params,
+        data=params,
         timeout=10
     )
     response.raise_for_status()
 except requests.exceptions.HTTPError as http_err:
     print(f"HTTP error occurred: {http_err}")
 except requests.exceptions.RequestException as req_err:
-    print(f"Request error occurred: {req_err}.\n\n{pvo_url}?"
-        "{urlencode(params)}"
+    print(
+        f"Request error occurred: {req_err}.\n\n"
+        f"{PVO_BASE_URL}/service/r2/addstatus.jsp?{urlencode(params)}"
     )
